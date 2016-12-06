d23063786a21352e37187d93ab6a99b7c367e056
Narde98 <andrea.nardella@hotmail.it>
2016-12-06 15:40:53 +0100

    Edited with the whole K team
    it places SPSs correctly
    added but not tested a flyaround function
    testing item choosing later

--- Alliance proposal for std code.cpp
+++ Alliance proposal for std code.cpp
@@ -32,13 +32,11 @@ float ps[3];
 float point[3];
 
 
-float   myState[12];    //status
 float   itemState[12];
 float   itemAtt[3];
 float   pointAtt[3];
 float   dockDist[3];//L|M|S
 
-float   cube_pos[3];
 float   actualTarget[3];
 float   distFromTarget;
 int     itemNumber;
@@ -57,7 +55,7 @@ bool    check;
 bool imBlue;//which shphere we are
 int phase;  //just to keep track of what do we have to do
 
-static class General{
+class General{
 public:
     static void copyVec(float *src,float *dest,int inPos,int dim){
         for(int i = inPos;i < dim;i++)
@@ -71,7 +69,7 @@ public:
     static float dist(float pos1[], float pos2[]){
         float tempVec[3];
         mathVecSubtract(tempVec,pos1,pos2,3);
-        return (mathVecMagnitude(tempVec,3));
+        return mathVecMagnitude(tempVec,3);
     }
     static bool compare(float a,float b,float appr){
         return a-b <= appr;
@@ -84,48 +82,98 @@ public:
     }
 };
 
-
-class {//movement
+class Movement{//movement
 private:
     float target [3]; //the point that has to be reached
     int mode;//how will it go (setPosTarget/forces/ecc...)
+    int ID;
+    float status[12];
 public:
-    void everySec();
+    void everySec(int id, float state[]){
+        ID=id;
+        General::copyVec(state, status, 0, 12);
+        switch(mode){
+            case 0:
+                api.setPositionTarget(target);
+                break;
+            case 1:
+                forces();
+                break;
+            case 2:
+                flyaround();
+                break;
+        }
+    }
     void setDestination(float dest[],int modality){
-        General.copyVec(dest,target,0,3);
+        General::copyVec(dest,target,0,3);
         mode=modality;
     }
     void getDestination(float dest[]){
-        General.copyVec(target,dest,0,3);
+        General::copyVec(target,dest,0,3);
+    }
+    void forces();
+    void flyaround()
+    {
+        float itemState[12];
+        game.getItemZRState(itemState,ID);
+        float item_att[3];
+    
+        for(int i =0; i<3; ++i)
+            item_att[i] = itemState[6+i];
+    
+        float dist[3];
+        float max=-1;
+        int max_num=-1;
+        for(int i = 0; i<3; ++i){
+            dist[i] = fabsf(status[i]-itemState[i]);
+            if (dist[i]>max)
+            {
+                max = dist[i];
+                max_num = i;
+            }
+        }
+    
+        float force_point[3];
+        for(int i =0; i<3;++i)
+            force_point[i] = item_att[i];
+        force_point[max_num]*=-1;
+        forces();
+        //counter++;
+        //if (counter % 2 == 0) api.setForces(force_point);
     }
 }move;
 
 void rotate(float target[3]){
     float tempVec[3];
-    mathVecSubtract(tempVec, target, myPos, 3);
+    mathVecSubtract(tempVec, target, pos, 3);
     mathVecNormalize(tempVec, 3);
     api.setAttitudeTarget(tempVec);
 }
 
 void drawTriangle(bool imBlue){
     float target[3];
-    if(game.getNumSPSHeld()==2)// 2nd point
+    if(game.getNumSPSHeld()>=2){// 2nd point
+        DEBUG(("select target1"));
         if(imBlue)
-            General.assign(target, 0.65, -0.65, 0.0);
+            General::assign(target, 0.6, -0.6, 0.0);
         else
-            General.assign(target, 0.65, 0.5, 0.5);
-
-    else if (game.getNumSPSHeld()==1){ //last point
+            General::assign(target, -0.6, 0.6, 0.0);
+    }
+    else if (game.getNumSPSHeld()>=1){ //last point
+        DEBUG(("select target2"));
         if(imBlue)
-            General.assign(target, 0.65, -0.65, 0.0);
+            General::assign(target, 0.6, 0.5, 0.5);
         else
-            General.assign(target, 0.65, -0.5, -0.5);
-
+            General::assign(target, 0.6, -0.5, -0.5);
+    }
     move.setDestination(target,SPS);
-    
+    DEBUG(("move %f",General::dist(target,myState) ));
     //if close enough to the point, drop it
-    if(General.dist(target,pos)<0.1)
+    if(General::dist(target,myState)<0.08){
         game.dropSPS();
+        DEBUG(("drop!"));
+    }
+    
 }
 
 
@@ -138,22 +186,21 @@ void init(){
 
 void loop(){
     api.getMyZRState(myState);
-    if(game.getNumSPSHeld())//if you still have some SPSs
+    if(game.getNumSPSHeld()>0){//if you still have some SPSs
         drawTriangle(imBlue);
+        DEBUG(("SPS draw triangle"));
+    }
     else{
         switch(phase){
             case 0:
                 game.getZone(ourZone);
-                General.assign(theirZone,-ourZone[0],
+                General::assign(theirZone,-ourZone[0],
                                -ourZone[1],-ourZone[2]);
                 phase++;
             break;
             case 1:
             break;
-
-            default:
-                phase=phase/0;//throw an error and abort
         }
     }
-    move.everySec();
+    move.everySec(id, myState);
 }
