d61cc42092c021dcb59e5ff41e0ac289055da407
LucaMazza99 <thelucraft99@gmail.com>
2016-12-13 14:32:58 +0100

    WIP (Not Working)

--- Alliance proposal for std code.cpp
+++ Alliance proposal for std code.cpp
@@ -1,62 +1,16 @@
-//proposal for std code
-
-#define s1 0.162
-#define s2 0.149
-#define s3 0.135 //still don't like those 3 consts
-
-//movement modes
-#define SPS 0
-#define ITEM 1
-#define GOAROUND 2
-
-int sps; //game.getSPSRemaining() or similar
-float cubeposes[6][3];
-
-float pos[3];
-float myState[12];
-float att[3];
-float cube_pos[3];
-float s;
-
-
-bool flag1, flag3;
-bool flag;//flag...very explicative
-bool team;
-bool trianglend;
-bool freep;
-bool first;
-bool indo;
-
-int id;
-float ps[3];
-float point[3];
-
-
-float   itemState[12];
-float   itemAtt[3];
-float   pointAtt[3];
-float   dockDist[3];//L|M|S
-
-float   actualTarget[3];
-float   distFromTarget;
-int     itemNumber;
-float   distMin;
-float   distMax;
-
-float   ranking[4];
-
-float   ourZone[4];     //assembly zones
-float   theirZone[3];
-
-char    index;    //switch index
-bool    check;
-
-
-bool imBlue;//which shphere we are
-int phase;  //just to keep track of what do we have to do
-
+//Begin page General
 class General{
 public:
+    static bool imBlue;         //which shphere we are
+    static float pos[3];
+    static float myState[12];
+    static float ourZone[4];    //assembly zones
+    static int id;
+    static float target[3];
+    static float theirZone[3];
+    static float otState[12];
+    static float dockDist[3]};   //L|M|S, TODO
+    
     static void copyVec(float *src,float *dest,int inPos,int dim){
         for(int i = inPos;i < dim;i++)
             dest[i]=src[i];
@@ -73,7 +27,6 @@ public:
     }
     static bool compare(float a,float b,float appr){
         return a-b <= appr;
-        //I needed this line to fold the function...lol
     }
     static bool compareVec(float a[], float b[], float appr){
         return a[0]<b[0]+appr && a[0]>b[0]-appr &&
@@ -81,17 +34,52 @@ public:
                a[2]<b[2]+appr && a[2]>b[2]-appr;
     }
 };
+//End page General
+//Begin page main
+//proposal for std code
 
-class Movement{//movement
-private:
-    float target [3]; //the point that has to be reached
-    int mode;//how will it go (setPosTarget/forces/ecc...)
-    int ID;
-    float status[12];
+
+
+void init(){
+    api.getMyZRState(General::myState);
+    api.getOtherZRState(General::otState);
+    game.dropSPS();//1st point to build the triangle
+    General::imBlue=General::myState[1]>0;
+    drawTriangle(General::imBlue);
+}
+
+void loop(){
+    api.getMyZRState(General::myState);
+    if(game.getNumSPSHeld()>0){//if you still have some SPSs
+        drawTriangle(General::imBlue);
+        DEBUG(("SPS draw triangle"));
+    }
+    else{
+        game.getZone(General::ourZone);
+        General::assign(General::theirZone,-General::ourZone[0],-General::ourZone[1],-General::ourZone[2]);
+        
+        game.getItemLoc(General::target,objSel.getID());
+        move.setDestination(General::target,ITEM);//SPS just for now, then ITEM or FLYAROUND
+    }
+
+    move.everySec();
+}
+
+//End page main
+//Begin page movement
+//movement modes
+#define SPS 0
+#define ITEM 1
+#define GOAROUND 2
+
+
+class {//movement
+    float target [3];   //the point that has to be reached
+    int mode;           //how will it go (setPosTarget/forces/ecc...)
+    float startDist;
+    float curDist;
 public:
-    void everySec(int id, float state[]){
-        ID=id;
-        General::copyVec(state, status, 0, 12);
+    void everySec(){
         switch(mode){
             case 0:
                 api.setPositionTarget(target);
@@ -105,47 +93,66 @@ public:
         }
     }
     void setDestination(float dest[],int modality){
-        General::copyVec(dest,target,0,3);
+        getDestination(dest);
         mode=modality;
     }
     void getDestination(float dest[]){
-        General::copyVec(target,dest,0,3);
+        General::copyVec(dest,target,0,3);
+        startDis = General::dist(myState,target);
     }
-    void forces();
-    void flyaround()
-    {
+    void forces(){
+        float forceV[3];
+        General::assign(forceV,target[0]-myState[0],target[1]-myState[1],target[2]-myState[2]);
+        curDist = General::dist(myState,targ_absolute);
+        
+        DEBUG(("curDis is %f \n", curDist));
+        if (curDist>=0.009)
+        {
+            if (curDist>startDist)
+                api.setForces(forceV);
+            else
+                api.setPositionTarget(target);
+        }
+        else
+        {
+            DEBUG(("arrived"));
+        }
+    }
+    
+    void flyaround(){
         float itemState[12];
         game.getItemZRState(itemState,ID);
-        float item_att[3];
+        float itemAtt[3];
     
         for(int i =0; i<3; ++i)
-            item_att[i] = itemState[6+i];
+            itemAtt[i] = itemState[6+i];
     
         float dist[3];
         float max=-1;
-        int max_num=-1;
+        int maxNum=-1;
         for(int i = 0; i<3; ++i){
-            dist[i] = fabsf(status[i]-itemState[i]);
+            dist[i] = fabsf(General::myState[i]-itemState[i]);
             if (dist[i]>max)
             {
                 max = dist[i];
-                max_num = i;
+                maxNum = i;
             }
         }
     
-        float force_point[3];
+        float forcePoint[3];
         for(int i =0; i<3;++i)
-            force_point[i] = item_att[i];
-        force_point[max_num]*=-1;
+            forcePoint[i] = itemAtt[i];
+        forcePoint[maxNum]*=-1;
         forces();
         //counter++;
         //if (counter % 2 == 0) api.setForces(force_point);
     }
 }move;
-
+//End page movement
+//Begin page rotate && triangle
 void rotate(float target[3]){
     float tempVec[3];
-    mathVecSubtract(tempVec, target, pos, 3);
+    mathVecSubtract(tempVec, General::target, General::pos, 3);
     mathVecNormalize(tempVec, 3);
     api.setAttitudeTarget(tempVec);
 }
@@ -155,52 +162,65 @@ void drawTriangle(bool imBlue){
     if(game.getNumSPSHeld()>=2){// 2nd point
         DEBUG(("select target1"));
         if(imBlue)
-            General::assign(target, 0.6, -0.6, 0.0);
+            General::assign(General::target, 0.6, -0.6, 0.0);
         else
-            General::assign(target, -0.6, 0.6, 0.0);
+            General::assign(General::target, -0.6, 0.6, 0.0);
     }
     else if (game.getNumSPSHeld()>=1){ //last point
         DEBUG(("select target2"));
-        if(imBlue)
+        if(General::imBlue)
             General::assign(target, 0.6, 0.5, 0.5);
         else
             General::assign(target, 0.6, -0.5, -0.5);
     }
-    move.setDestination(target,SPS);
-    DEBUG(("move %f",General::dist(target,myState) ));
+    move.setDestination(General::target,SPS);
+    DEBUG(("move %f",General::dist(General::target,General::myState) ));
     //if close enough to the point, drop it
-    if(General::dist(target,myState)<0.08){
+    if(General::dist(General::target,General::myState)<0.08){
         game.dropSPS();
         DEBUG(("drop!"));
     }
     
 }
 
-
-void init(){
-    api.getMyZRState(myState);
-    game.dropSPS();//1st point to build the triangle
-    imBlue=myState[1]>0;
-    drawTriangle(imBlue);
-}
-
-void loop(){
-    api.getMyZRState(myState);
-    if(game.getNumSPSHeld()>0){//if you still have some SPSs
-        drawTriangle(imBlue);
-        DEBUG(("SPS draw triangle"));
+//End page rotate && triangle
+//Begin page select object
+class {
+    int itemChosen;
+    int objID;
+    float objIDPos[3], objIDp1Pos[3]; //ObjIDPos = pos of objID; objIDp1Pos = pos of objID+1
+    float myState[12];
+    float otState[12];
+    bool isNearer(float objPos[]) {
+        api.getMyZRState(myState);
+        api.getOtherZRState(General::otState);
+        if (!(game.hasItem(objID) != 2 || game.itemInZone(objID))) //Is obj free? 
+            if (General::dist(myState, objPos) > General::dist(General::otState, objPos)) //Am i further than other?
+                return false;   //I am further
+            else
+                return true;    //I am nearer
+        else
+            return false;       //Obj isn't free
+    }        
+public:
+    int getID(){
+        itemChosen = -1; 
+        objID=0;
+        do {
+            game.getItemLoc(objIDPos, objID);
+            game.getItemLoc(objIDp1Pos, objID+1);
+            
+            if (!isNearer(objIDPos))
+                if (!isNearer(objIDp1Pos))
+                    objID+=2;
+                else
+                    itemChosen = objID+1;
+            else
+                itemChosen = objID;
+                
+        } while(itemChosen == -1);
+        
+        return itemChosen;
     }
-    else{
-        switch(phase){
-            case 0:
-                game.getZone(ourZone);
-                General::assign(theirZone,-ourZone[0],
-                               -ourZone[1],-ourZone[2]);
-                phase++;
-            break;
-            case 1:
-            break;
-        }
-    }
-    move.everySec(id, myState);
-}
+}objSel;
+//End page select object
